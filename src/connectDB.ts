0169f569ea46c4b8652bd37c1459fa0914c10d99
Ram Krishna Yadav <itsramky234@gmail.com>
2025-04-29 18:52:32 +0545

    MongoUrl Added

--- src/connectDB.ts
+++ src/connectDB.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose"
+import "dotenv"
 
 export default function connectDB() {
-    const mongo_url = process.env.MONGO_URL || "mongodb://localhost:27017/test"
-    try {
+    const mongo_url = process.env.MONGO_URI || "mongodb://localhost:27017/test"
+        try {
         mongoose.connect(mongo_url)
         console.log("Connection to database succesfull.")
     } catch {
